944ceb2422c489f77d17e687ed2d87caeb7c9905
Koray_Amico_Kulbay <kkulbay@kth.se>
2023-10-02 17:57:24 +0200

    :construction: path shows in rviz, need debug

--- planning/multi_agent/scripts/path_pattern_generator.py
+++ planning/multi_agent/scripts/path_pattern_generator.py
@@ -51,7 +51,7 @@ class PatternGenerator():
         rospy.loginfo("Received goal")
         self.rect_height = self.distance_hugin_0_to_goal(msg)
         print("rect_height: ", self.rect_height)
-        # self.generate_lawn_mower_pattern()
+        self.generate_lawn_mower_pattern()
 
     
     def distance_hugin_0_to_goal(self, goal):
@@ -89,12 +89,41 @@ class PatternGenerator():
         center_y=self.center_y,
         exiting_line=self.exiting_line
         )
+        print(timed_paths_list)
+
+        # Publish waypoints as Path messages for each AUV
+        for agent_idx, timed_path in enumerate(timed_paths_list):
+            path_msg = Path()
+            path_msg.header.stamp = rospy.Time.now()
+            path_msg.header.frame_id = "map"  # Assuming the waypoints are in the map frame
+
+            # Create PoseStamped messages for each waypoint
+            for waypoint in timed_path.wps:
+                pose_stamped = PoseStamped()
+                pose_stamped.header = path_msg.header
+                pose_stamped.pose.position.x = waypoint.pose[0]
+                pose_stamped.pose.position.y = waypoint.pose[1]
+                heading = waypoint.pose[2]
+                #TODO: Convert heading to quaternion and add to pose_stamped
+                path_msg.poses.append(pose_stamped)
+
+            # Publish the Path message for this agent
+            agent_path = AgentPath()
+            agent_path.agent_id = agent_idx  # Set a unique ID for each agent
+            agent_path.path = path_msg
+            self.paths.path_array.append(agent_path)
+
+        # Publish AgentPathArray containing all agent paths
+        self.paths.header.stamp = rospy.Time.now()
+        self.paths.header.frame_id = "map"  # Assuming the paths are in the map frame
+        self.path_array_pub.publish(self.paths)
+        print("Published paths")
 
         # rospy.loginfo("Publishing AgentPathArray")
         # self.path_array_pub.publish(self.paths)
-        # #Reset
-        # self.paths = AgentPathArray()
-        # self.goal = None
+        #Reset
+        self.paths = AgentPathArray()
+        self.goal = None
 
     
 
